6e0cbbbcb07d3c368272034d2d3917414efc7fce
Lee A. Mitchell <2857651+LeeAMitchell@users.noreply.github.com>
2026-09-09 23:09:38 -0400

    WIP: Add support for 1592EMS1.213 (Prestige 16Evo A13MX)

--- msi-ec.c
+++ msi-ec.c
@@ -1662,6 +1662,70 @@ static struct msi_ec_conf CONF_G2_6 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_G2_7[] __initconst = {
+	"1592EMS1.213", // Prestige 16Evo A13MX
+	NULL
+};
+
+static struct msi_ec_conf CONF_G2_7 __initdata = {
+	.allowed_fw = ALLOWED_FW_G2_7,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = 0x2f,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+        .invert  = true,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xd2,
+		.modes = {
+			{ SM_TURBO_NAME,   0xc4 },
+			{ SM_ECO_NAME,     0xc2 },
+			{ SM_COMFORT_NAME, 0xc1 },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+	    .address = MSI_EC_ADDR_UNSUPP
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+	    .rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = 0x2c,
+	    .bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = 0xd3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static const char *ALLOWED_FW_G2_10[] __initconst = {
 	"1562EMS1.117", // Stealth 15M A11SEK
 	"1563EMS1.106", // Stealth 15M A11UEK
@@ -1847,6 +1911,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF_G2_4,
 	&CONF_G2_5,
 	&CONF_G2_6,
+	&CONF_G2_7,
 	&CONF_G2_10,
 	NULL
 };
